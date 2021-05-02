172d2c1593c4da8cffbdbf6c2806f95864a9cc97
Foyzul Abrar Chowdhury <Foyzul.Abrar@gmail.com>
2021-05-02 11:04:46 +0600

    Add project files.

---
+++ JWT_CORE/Controllers/NameController.cs
@@ -0,0 +1,30 @@
+﻿using JWT_CORE.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JWT_CORE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        // GET: api/<NameController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserDTO userDTO)
+        {
+            return Ok();
+        }
+
+    }
+}
